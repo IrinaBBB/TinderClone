915359e0a087cd06f42f784d25032b38e2acdc47
Irina <balkarovairina@gmail.com>
2021-07-24 17:31:13 +0200

    add: layout for StartupActivity

--- app/src/main/java/ru/irinavb/tinderclone/activities/StartupActivity.kt
+++ app/src/main/java/ru/irinavb/tinderclone/activities/StartupActivity.kt
@@ -2,6 +2,7 @@ package ru.irinavb.tinderclone.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import ru.irinavb.tinderclone.databinding.ActivityStartupBinding
 
 class StartupActivity : AppCompatActivity() {
@@ -13,5 +14,15 @@ class StartupActivity : AppCompatActivity() {
         binding =  ActivityStartupBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
+
+        // binding.login.setOnClickListener(onLogin(this))
+    }
+
+    fun onLogin(v: View) {
+
+    }
+
+    fun onSignup(v: View) {
+
     }
 }
