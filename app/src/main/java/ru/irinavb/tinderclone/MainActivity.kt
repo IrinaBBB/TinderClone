55cd178362a743dc4e94980473dc3f4ef47e0659
IrinaBBB <balkarovairina@gmail.com>
2021-07-24 12:24:19 +0200

    Initial

---
+++ app/src/main/java/ru/irinavb/tinderclone/MainActivity.kt
@@ -0,0 +1,11 @@
+package ru.irinavb.tinderclone
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
