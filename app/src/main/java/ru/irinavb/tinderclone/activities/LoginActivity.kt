4005bc396900ec6c5d0c498a2e0c855ec1477a48
Irina <balkarovairina@gmail.com>
2021-07-25 18:37:34 +0200

    add: login functionality

--- app/src/main/java/ru/irinavb/tinderclone/activities/LoginActivity.kt
+++ app/src/main/java/ru/irinavb/tinderclone/activities/LoginActivity.kt
@@ -4,11 +4,22 @@ import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.ktx.Firebase
 import ru.irinavb.tinderclone.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityLoginBinding
+
+    private val firebaseAuth = FirebaseAuth.getInstance()
+    private val firebaseAuthListener = FirebaseAuth.AuthStateListener {
+        val user = firebaseAuth.currentUser
+        if (user != null) {
+            startActivity(MainActivity.newIntent(this))
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,7 +31,29 @@ class LoginActivity : AppCompatActivity() {
     }
 
     private fun onLogin() {
-        startActivity(MainActivity.newIntent(this))
+        val email = binding.emailEditText.text.toString()
+        val password = binding.passwordEditText.text.toString()
+
+        if (email.isNotEmpty() && password.isNotEmpty()) {
+            firebaseAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener {task ->
+                    if(!task.isSuccessful) {
+                        Toast.makeText(this,
+                            "Login error ${task.exception?.localizedMessage}", Toast.LENGTH_LONG)
+                            .show()
+                    }
+                }
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        firebaseAuth.addAuthStateListener(firebaseAuthListener)
+    }
+
+    override fun onStop() {
+        super.onStop()
+        firebaseAuth.removeAuthStateListener(firebaseAuthListener)
     }
 
     companion object {
