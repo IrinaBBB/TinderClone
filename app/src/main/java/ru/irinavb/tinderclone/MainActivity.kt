bea65d566ccd0037204ff1066f47df03a182cef7
Irina <balkarovairina@gmail.com>
2022-05-03 18:54:21 +0200

    add: initial setup for swiping cards

--- app/src/main/java/ru/irinavb/tinderclone/MainActivity.kt
+++ app/src/main/java/ru/irinavb/tinderclone/MainActivity.kt
@@ -1,11 +1,66 @@
 package ru.irinavb.tinderclone
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.ArrayAdapter
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.lorentzos.flingswipe.SwipeFlingAdapterView
+import ru.irinavb.tinderclone.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    private var al = ArrayList<String>()
+    private var arrayAdapter: ArrayAdapter<String>? = null
+    private var i = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        al = ArrayList<String>().apply {
+            add("php")
+            add("c")
+            add("python")
+            add("java")
+        }
+
+        arrayAdapter = ArrayAdapter<String>(this, R.layout.item_card, R.id.helloText, al)
+
+        binding.frame.adapter = arrayAdapter
+        binding.frame.setFlingListener(object : SwipeFlingAdapterView.onFlingListener {
+            override fun removeFirstObjectInAdapter() {
+                Log.d(TAG, "removed object!")
+                al.removeAt(0)
+                arrayAdapter?.notifyDataSetChanged()
+            }
+
+            override fun onLeftCardExit(dataObject: Any) {
+                Toast.makeText(this@MainActivity, "Left!", Toast.LENGTH_SHORT).show()
+            }
+
+            override fun onRightCardExit(dataObject: Any) {
+                Toast.makeText(this@MainActivity, "Right!", Toast.LENGTH_SHORT).show()
+            }
+
+            override fun onAdapterAboutToEmpty(itemsInAdapter: Int) {
+                al.add("XML $i")
+                arrayAdapter?.notifyDataSetChanged()
+                Log.d(TAG, "notified")
+                i++
+            }
+
+            override fun onScroll(p0: Float) {
+            }
+        })
+    }
+
+    companion object {
+        private const val TAG = "LIST"
     }
 }
+
+
